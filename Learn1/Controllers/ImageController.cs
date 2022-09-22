138c93252b60e22b561425272752392200fe0175
ducmtgch200681 <ducmtgch200681@fpt.edu.vn>
2022-09-22 12:17:32 +0700

    ok

---
+++ Learn1/Controllers/ImageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learn1.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult CC()
+        {
+            return View();
+        }
+    }
+}
