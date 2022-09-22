0f305983d5590c85147f736ca30319ace2851fae
ducmtgch200681 <ducmtgch200681@fpt.edu.vn>
2022-09-22 11:26:34 +0700

    ok

---
+++ Learn1/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learn1.Controllers
+{
+    public class DemoController : Controller
+    {
+        /*
+        Perpose: Render file view
+        place: Views/Demo/Index.cshtml
+        "Demo": controller name (class)
+        "Index": Action name (method) 
+        */
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/")]
+        public IActionResult ARandomGrace()
+        {
+            return View();
+        }
+    }
+}
